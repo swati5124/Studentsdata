964edd1ea0eb584007cb1859d528f20b00612ba6
swati5124 <swati@niit-tech.com>
2020-04-03 11:07:25 +0530

    Adding Controller

--- gitignoree3april/Controllers/StudentController.cs
+++ gitignoree3april/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using gitignoree3april.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gitignoree3april.Controllers
@@ -10,7 +11,15 @@ namespace gitignoree3april.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Student> obj = new List<Student>() {
+           new Student  { SId = 101, Name = "Sam" },
+
+
+            new Student  { SId = 102, Name = "Abha" }
+        };
+             return View(obj);
+
+        }
+           
         }
     }
-}
---
+++ gitignoree3april/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gitignoree3april.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string Name { get; set; }
+       
+       
+
+    }
+}
